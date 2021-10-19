65ecffa19a13eae3f80d4ef4cb976de9ffb31473
ibakaito <e1b19009@oit.ac.jp>
2021-10-19 14:30:52 +0900

    first commit

---
+++ src/main/java/ydrive/db_ydrive/DbYdriveApplication.java
@@ -0,0 +1,13 @@
+package ydrive.db_ydrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbYdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbYdriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ydrive/db_ydrive/DbYdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package ydrive.db_ydrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbYdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
